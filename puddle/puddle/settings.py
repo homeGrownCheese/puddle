14b7bd865aef68ea2c5c864abe57a9678face318
homegrowncheese <shanis9er@me.com>
2023-02-03 12:23:36 -0800

    items

--- puddle/puddle/settings.py
+++ puddle/puddle/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'item',
     'core',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -117,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MIEDIA_URL = 'media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
