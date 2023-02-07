97361788890c726e8af4b54bbbd0416d3444bff5
homegrowncheese <shanis9er@me.com>
2023-02-07 11:23:11 -0800

    item details page

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -4,12 +4,11 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-from core.views import index, contact
-
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('contact/', contact, name='contact'),
+    path('', include('core.urls')),
+    path('items/', include('item.urls')),
     path('admin/', admin.site.urls),
+
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
